3b22a4adf9d67d3e4f99bd12994e44000afa2b4b
Nicolas Bayle <nicolas.bayle.it@gmail.com>
2019-03-02 19:13:26 +0000

    20190302: tested

--- osProvider.sh
+++ osProvider.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 echo "Copying the github repos"
 ansible-playbook git.yml
-# echo "#####################################"
-# echo "Copying the Ansible hostfile"
-# cp hostsLocal devstack/hosts
-# cp hostsLocal devstack/aviKvm
-# echo "#####################################"
-# echo "Copying the config files"
-# cp params_devstack.yml devstack/vars/params.yml
-# cp params_aviKvm.yml aviKvm/vars/params.yml
-# cp params_aviBootstrap.yml aviBootstrap/vars/params.yml
-# cp params_aviOs.yml aviOs/vars/params.yml
 echo "#####################################"
 echo "Adding route to the ansible host"
 sudo ip route add 200.1.1.11/32 via 192.168.139.135 || true
